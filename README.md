399526204783627ad16397a279a8c703a720ab65
chinyasuhail <suhail@chinyasuhail.com>
2021-07-13 01:16:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-ubzmz7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-ubzmz7)
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col mx-auto p-2 bg-black items-center">
+      <img src="uni.svg" className="ml-4" />
+    </div>
+  );
+}
