2483292a6d7f1c4e155356214a742439a5336416
chinyasuhail <suhail@chinyasuhail.com>
2021-07-13 09:59:51 +0530

    added hero image

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,23 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
+
+
+<div>
+
     <div className="flex flex-col mx-auto p-2 bg-black items-center">
       <img src="uni.svg" className="ml-4" />
     </div>
+
+  <div className="flex flex-col mx-auto p-2 bg-black items-center h-screen bg-right bg-hero-pattern bg-no-repeat bg-contain"> 
+  </div>
+
+  <div className="flex flex-col mx-auto p-2 bg-blue-100 items-center">
+      <img src="uni.svg" className="ml-4" />
+    </div>
+
+
+  </div>
+
   );
 }
