b2476991c6bf54aee0baf6ef1ef35727bebcf5af
chinyasuhail <suhail@chinyasuhail.com>
2021-07-13 10:26:41 +0530

    remove eslint

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-    /* config options here */
+    eslint: {
+      // Warning: Dangerously allow production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
   }
